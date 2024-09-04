db7bc424529095a94d0de087b81addfe02af3f2c
gasungfa <gasungfa@gmail.com>
2024-09-04 21:07:31 +0900

    baekjoon 2750 solution

---
+++ 2750/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int num[1000];
+
+int main(){
+    int N = 0;
+    cin >> N;
+
+    for(int i = 0;i<N;i++){
+        cin>>num[i];
+    }
+    sort(num, num+N);
+    for(int i = 0;i<N;i++){
+        cout<<num[i]<<"\n";
+    }
+    return 0;
+}
